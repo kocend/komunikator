823ad338f82e4d6b1f9837d2177c94c03238ede5
kocend <48856972+kocend@users.noreply.github.com>
2019-05-28 11:02:02 +0200

    Add files via upload

--- src/chatClient/ConversationScene.java
+++ src/chatClient/ConversationScene.java
@@ -1,7 +1,7 @@
 package chatClient;
 
+import java.util.HashSet;
 import java.util.LinkedList;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -17,69 +17,82 @@ import javafx.scene.text.Text;
 
 
 public class ConversationScene {
-	
-	private Gui gui;
-	private Scene conversationScene;
-	private TextField typingField;
-	private TextArea conversationArea;
-	private LinkedList<String> activeUsers;
-	
-	public ConversationScene(Gui obj) {
-		
-		gui=obj;
-		
-		Text areaLabel = new Text("rozmowa:");
-	    Text onlineLabel = new Text("online:"); 
-	       
-	    conversationArea = new TextArea();
-	    conversationArea.setPrefSize(300, 300);
-	    conversationArea.setWrapText(true);
-	    conversationArea.setEditable(false);
-	    
-	    activeUsers = new LinkedList<>();
-	    ListView<String> onlineUsers = new ListView<>();
-	    onlineUsers.setPrefSize(20, 300);
-	    onlineUsers.getItems().addAll(activeUsers);
-	    
-	    
-	    typingField = new TextField();
-	    typingField.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event)->{if(event.getCode()==KeyCode.ENTER)gui.sendText();});
-	     
-	    Button sendButton = new Button("wyœlij"); 
-	    sendButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event)->{gui.sendText();});
-	    
-	    GridPane gridPane = new GridPane();
-	    gridPane.setPadding(new Insets(10, 10, 10, 10));
-	    gridPane.setVgap(10);
+
+    private Gui gui;
+    private Scene conversationScene;
+    private TextField typingField;
+    private TextArea conversationArea;
+    private HashSet<String> activeUsers;
+    private ListView<String> onlineUsers;
+
+    public ConversationScene(Gui obj) {
+
+        gui=obj;
+
+        Text areaLabel = new Text("rozmowa:");
+        Text onlineLabel = new Text("online:");
+
+        conversationArea = new TextArea();
+        conversationArea.setPrefSize(300, 300);
+        conversationArea.setWrapText(true);
+        conversationArea.setEditable(false);
+
+        activeUsers = new HashSet<>();
+        onlineUsers = new ListView<>();
+        onlineUsers.setPrefSize(60, 300);
+        onlineUsers.getItems().addAll(activeUsers);
+
+
+        typingField = new TextField();
+        typingField.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event)->{if(event.getCode()==KeyCode.ENTER)gui.sendText();});
+
+        Button sendButton = new Button("wyślij");
+        sendButton.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event)->{gui.sendText();});
+
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(10);
         gridPane.setHgap(10);
-	    gridPane.setAlignment(Pos.CENTER);
-	     
-	    gridPane.add(areaLabel, 0, 0); 
-	    gridPane.add(onlineLabel, 1, 0); 
-	    gridPane.add(conversationArea, 0, 1);       
-	    gridPane.add(onlineUsers, 1, 1); 
-	    gridPane.add(typingField, 0, 2); 
-	    gridPane.add(sendButton, 1, 2);
-	    
-	    gridPane.setStyle("-fx-background-color: rgba(100, 149, 237,0.4);");
-	    
-	    conversationScene = new Scene(gridPane);
-	    typingField.requestFocus();
-	}
-	
-	public Scene getInstance() {
-		return conversationScene;
-	}
+        gridPane.setAlignment(Pos.CENTER);
 
-	public TextField getTypingField() {
-		return typingField;
-	}
+        gridPane.add(areaLabel, 0, 0);
+        gridPane.add(onlineLabel, 1, 0);
+        gridPane.add(conversationArea, 0, 1);
+        gridPane.add(onlineUsers, 1, 1);
+        gridPane.add(typingField, 0, 2);
+        gridPane.add(sendButton, 1, 2);
 
-	public TextArea getConversationArea() {
-		return conversationArea;
-	}
+        gridPane.setStyle("-fx-background-color: rgba(100, 149, 237,0.4);");
 
-	public LinkedList<String> getActiveUsers() {
-		return activeUsers;
-	}
+        conversationScene = new Scene(gridPane);
+        typingField.requestFocus();
+    }
+
+    public Scene getInstance() {
+        return conversationScene;
+    }
+
+    public TextField getTypingField() {
+        return typingField;
+    }
+
+    public TextArea getConversationArea() {
+        return conversationArea;
+    }
+
+    public void addUser(String name){
+        activeUsers.add(name);
+        updateActiveUsers();
+    }
+
+    public void removeUser(String name) {
+        activeUsers.remove(name);
+        updateActiveUsers();
+    }
+
+    private void updateActiveUsers(){
+        onlineUsers.getItems().clear();
+        onlineUsers.getItems().addAll(activeUsers);
+    }
+
 }
